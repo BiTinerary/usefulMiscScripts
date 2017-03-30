a69e23b9e54f3ae3658c28005c2793c9d0d9ba0d
Izaac Kirscht <izaac.kirscht@gmail.com>
2017-03-29 22:10:37 -0500

    Update regexIPGetterScraper.py

--- regexIPGetterScraper.py
+++ regexIPGetterScraper.py
@@ -1,7 +1,7 @@
 import requests, pyperclip, re
 
-ratEnterprisesString = requests.get('')
-daGoodz = ratEnterprisesString.content # get raw content, minus encoding *ish
+urlString = requests.get('')
+daGoodz = urlString.content # get raw content, minus encoding *ish
 
 iPRegex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}') # IP Regex, could be shorter but c/would also be "moms spaghetti"
 found = iPRegex.findall(daGoodz) # compare html string with regex.
