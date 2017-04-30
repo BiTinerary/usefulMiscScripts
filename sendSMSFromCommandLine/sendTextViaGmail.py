27d1fce1384ed50c9c23d773d97a2ca7bd30690a
Izaac <izaac.kirscht@gmail.com>
2017-04-30 16:14:10 -0500

    Update sendTextViaGmail.py

--- sendSMSFromCommandLine/sendTextViaGmail.py
+++ sendSMSFromCommandLine/sendTextViaGmail.py
@@ -1,5 +1,42 @@
 import smtplib, sys
 
+#subject = sys.argv[1]
+
+lines = [line.rstrip('\n') for line in open('gmailCreds.txt', 'r')]
+
+def emailCreds():
+  gmailAddress = lines[0] 
+  gmailPass = lines[1]
+  toMyPhone = lines[2]
+
+  return [gmailAddress, gmailPass, toMyPhone] 
+
+def sendEmail(emailContent):
+  gmailAddress, gmailPass, toMyPhone = emailCreds()
+  emailContent = """Subject: %s""" % emailContent #(subject.split(',')[0], subject.split(',')[1])
+
+  try:  
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmailAddress, gmailPass)
+    server.sendmail(gmailAddress, toMyPhone, emailContent)
+    server.close()
+    print 'Email sent!'
+    
+  except:  
+    print 'Something went wrong...'
+
+#sendEmail('timestamp')
+# from another script (same folder)
+# from sendSMSFromCommandLine import sendEmail()
+# sendEmail('String Here')
+
+
+
+
+""" Old, misplaced inputs
+import smtplib, sys
+
 subject = sys.argv[1]
 
 lines = [line.rstrip('\n') for line in open('gmailCreds.txt', 'r')]
@@ -27,3 +64,4 @@ def sendEmail():
     print 'Something went wrong...'
 
 sendEmail()
+"""
