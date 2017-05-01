d730c078425e5cedd760a9da404ace8f7dd69e6f
Izaac <izaac.kirscht@gmail.com>
2017-04-30 21:41:34 -0500

    Update sendSMSViaGmail.py

--- reedSwitchSendSMS/sendSMSViaGmail.py
+++ reedSwitchSendSMS/sendSMSViaGmail.py
@@ -1,25 +1,25 @@
-import smtplib, sys
-
-#subject = sys.argv[1]
+import smtplib
 
 lines = [line.rstrip('\n') for line in open('/home/stuxnet/reedSwitch/gmailCreds.txt', 'r')]
+#One liner for openning/reading each line of a hardcoded directory and .txt file.
 
-def emailCreds():
-  gmailAddress = lines[0]
-  gmailPass = lines[1]
-  toMyPhone = lines[2]
-  return [gmailAddress, gmailPass, toMyPhone]
+def emailCreds(): #Assign each line it's own variable, which correspond to email, password, and cell phone email address.
+  gmailAddress = lines[0] #In.
+  gmailPass = lines[1] #That.
+  toMyPhone = lines[2] #Order.
+  return [gmailAddress, gmailPass, toMyPhone] #Return these variables as array.
 
-def sendEmail(emailContent):
-  gmailAddress, gmailPass, toMyPhone = emailCreds()
-  emailContent = """Subject: %s""" % emailContent #(subject.split(',')[0], subject.split(',')[1])
-  try:
+def sendEmail(emailContent): #Take an arguement as input. This argument will be the messages content.
+  gmailAddress, gmailPass, toMyPhone = emailCreds() # little uneccesary but bite me. (Re?)Assign string variables to each index
+  emailContent = """Subject: %s""" % emailContent #"Subject:" is internally recognized by sendmail() (line 19)
+  
+  try: # exception if fail to send, connect, login, etc..
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
-    server.login(gmailAddress, gmailPass)
-    server.sendmail(gmailAddress, toMyPhone, emailContent)
+    server.login(gmailAddress, gmailPass) #Login credentials
+    server.sendmail(gmailAddress, toMyPhone, emailContent) #send... (from, to, message)
     server.close()
     print 'Email sent!'
     
   except:
-    print 'Something went wrong...'
+    print 'Something went wrong...' #debug
