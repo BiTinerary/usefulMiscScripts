bb4a6123d6b1ede607d520b1f3cd6e56c115dc81
Izaac <izaac.kirscht@gmail.com>
2017-04-30 21:30:06 -0500

    Update reedSwitchState.py

--- reedSwitchSendSMS/reedSwitchState.py
+++ reedSwitchSendSMS/reedSwitchState.py
@@ -1,12 +1,12 @@
 #!/bin/python
 import time, datetime
-from sendSMSViaGmail import sendEmail
+from sendSMSViaGmail import sendEmail #importing a personalized, custom script.
 from pyA20.gpio import gpio
 from pyA20.gpio import port
 
-def initiatePin():
+def initiatePin(): #Enable various functions and config for spefic GPIO on NanoPiNeoAir
 
-	gpio.init()
+	gpio.init()# Must be passed first before any other functions from GPIO library.
 
 	gpio.setcfg(port.PA6, gpio.OUTPUT)
 	gpio.input(port.PA6)
@@ -16,33 +16,33 @@ def initiatePin():
 	gpio.pullup(port.PA6, gpio.PULLDOWN) #Enable pull-down
 	gpio.pullup(port.PA6, gpio.PULLUP) #Enable pull-up
 
-def doorState():
-	doorStateValue = gpio.input(port.PA6)
+def doorState(): #Get the state of the door's reed switch.
+	doorStateValue = gpio.input(port.PA6) #input() is current state (High/Low) of GPIO
 	if doorStateValue == 0:
 		doorStateString = 'Closed'
 	elif doorStateValue == 1:
 		doorStateString = "Open"
 
-	return doorStateValue, doorStateString
+	return doorStateValue, doorStateString # Return tuple. One boolean, one string.
 
 def sendAlert():
-	getTimeStamp = datetime.datetime.now().strftime('%H:%M:%S %m-%d-%Y') 
+	getTimeStamp = datetime.datetime.now().strftime('%H:%M:%S %m-%d-%Y')# timestamp. concatenated string of current time and date
 	alertMessage = "Door Opened: %s" % getTimeStamp
 	print alertMessage
-	sendEmail(alertMessage)
-	time.sleep(120)
+	sendEmail(alertMessage) #pass "alertMessage" as argument to sendEmail. Which is an imported function from another script.
+	time.sleep(120) # This uses smptlib library to send a message, from my gmail account to the email address of my cell number.
 
-initiatePin()
-
-while True:
-	time.sleep(2)
-	print "Door is: %s" % doorState()[1]
-	print "Door Value: %s" % doorState()[0]
 	
-	if doorState()[0] == 0:
+initiatePin() #start of script
+while True: #Initiate loop
+	time.sleep(2) 
+	print "Door is: %s" % doorState()[1] #Log
+	print "Door Value: %s" % doorState()[0] #Log
+	
+	if doorState()[0] == 0: #Door is closed, do nothing.
 		pass
 	
-	elif doorState()[0] == 1:
+	elif doorState()[0] == 1: #Door is open, run "sendAlert" function.
 		sendAlert()
 	
-	print "\n"
+	print "\n" # pretty print
