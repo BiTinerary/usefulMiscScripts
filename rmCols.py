e1dc507ff3f921b0d032fc62172ebb3c8b7c76d8
Izaac <izaac.kirscht@gmail.com>
2017-05-31 13:54:06 -0500

    Update rmCols.py

--- rmCols.py
+++ rmCols.py
@@ -6,8 +6,8 @@ writer = csv.writer(open(sys.argv[1].rstrip('.csv')+'-Slim.csv', 'wb'))
 x = 0
 
 for line in reader:
-   newLine = [value for column, value in enumerate(line) if column in (8,9,95,18,56,63,21,26,64,94,48,88,90)]#(8,9,18,21,26,48,56,63,64,88,90,94,95)]
+   newLine = [value for column, value in enumerate(line) if column in (8,9,95,18,56,63,21,26,64,94,75,88,90)]#(8,9,18,21,26,48,56,63,64,88,90,94,95)]
    writer.writerow(newLine)
    print x
    x += 1
-writer.writerow(str(""",=sum(B2:B%s),,,=sum(E2:E%s),=sum(F2:F%s),,,=sum(I2:I%s),,,=sum(L2:L%s),,""" % (x,x,x,x,x)).split(','))
+writer.writerow(str(""",=sum(B2:B%s),,,=sum(E2:E%s),,,=sum(H2:H%s),=sum(I2:I%s),,,=sum(L2:L%s),,""" % (x,x,x,x,x)).split(','))
